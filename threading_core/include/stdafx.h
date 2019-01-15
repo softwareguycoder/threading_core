58d7054f2ee637dc18b4c74bb950fd036ab9d059
softwareguycoder <softwareguycoder@gmail.com>
2019-01-15 12:58:45 -0500

    15 Jan 2019 Added proper includes and typedefs to stdafx.h

--- threading_core/include/stdafx.h
+++ threading_core/include/stdafx.h
@@ -2,7 +2,17 @@
 #define __STDAFX_H__
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#include <../../debug_core/debug_core/include/debug_core.h>
+#ifndef OK
+#define OK		0		// Code to return to the operating system to indicate successful program termination
+#endif // OK
+
+#ifndef ERROR
+#define ERROR	-1		// Code to return to the operating system to indicate an error condition
+#endif // ERROR
+
+#include <../../../debug_core/debug_core/include/debug_core.h>
 
 #endif //__STDAFX_H__
