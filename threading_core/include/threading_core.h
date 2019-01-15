137cb45dfc19425ebcfe65756ab219915f982b25
softwareguycoder <softwareguycoder@gmail.com>
2019-01-15 12:36:00 -0500

    15 Jan 2019 Wrote an internal _FreeThread function

--- threading_core/include/threading_core.h
+++ threading_core/include/threading_core.h
@@ -1,6 +1,31 @@
 #ifndef __THREADING_CORE_H__
 #define __THREADING_CORE_H__
 
+#include <unistd.h>  //Header file for sleep(). man 3 sleep for details.
+#include <pthread.h>
 
+/**
+ * @brief Handle to a process thread.
+ */
+typedef pthread_t* HTHREAD;
+
+/**
+ * @brief Pointer to a function that will be executed for a given thread.
+ * @param lpThreadParameter A reference to memory containing user state.  May be NULL.
+ * @returns Reference to a block of memory containing user state.  May be NULL.
+ */
+typedef void* (*LPTHREAD_START_ROUTINE)(void* lpThreadParameter);
+
+#ifndef INVALID_HANDLE_VALUE
+#define INVALID_HANDLE_VALUE NULL
+#endif //INVALID_HANDLE_VALUE
+
+/**
+ * @brief Creates a new thread and returns a handle to it, or returns INVALID_HANDLE_VALUE if the
+ * operating system was unable to create a new thread.
+ * @param lpfnThreadProc Address of a function that will serve as the thread procedure.
+ * @return Handle to the created thread, or INVALID_HANDLE_VALUE if an error occurred.
+ */
+HTHREAD CreateThread(LPTHREAD_START_ROUTINE lpfnThreadProc);
 
 #endif //__THREADING_CORE_H__
--- threading_core/src/threading_core.c
+++ threading_core/src/threading_core.c
@@ -3,3 +3,44 @@
 
 #include "threading_core.h"
 
+///////////////////////////////////////////////////////////////////////////////
+// _FreeThread: Internal function for freeing malloc'd thread handles.  This
+// function is not exposed in the header file for this library, as it is
+// meant for internal use only.
+
+/**
+ * @brief Internal (i.e., will not be put in the mutex.h header file) method
+ * for freeing malloc'd thread handles.
+ * @param hThread The handle to be freed.
+ */
+void _FreeThread(HTHREAD hThread) {
+	log_info("In _FreeThread");
+
+	log_info("_FreeThread: Checking whether the thread handle passed is valid...");
+
+	if (INVALID_HANDLE_VALUE == hThread) {
+		log_warning(
+				"_FreeThread: The thread handle passed has an invalid value; assuming it's already been deallocated.");
+
+		log_info("_FreeThread: Done.");
+
+		// If we have an invalid handle (i.e., NULL pointer), then there is nothing to do.
+		return;
+	}
+
+	log_info(
+			"_FreeThread: The thread handle passed is valid.  Freeing the memory...");
+
+	// The HMUTEX handle type is just a typedef of pthread_mutex_t*
+	// However, to work with the pthread functions, we need to view it
+	// as such.
+	pthread_t* pThread = (pthread_t*) hThread;
+
+	free(pThread);
+	pThread = NULL;
+	hThread = INVALID_HANDLE_VALUE;
+
+	log_info("_FreeThread: The memory occupied by the thread handle passed has been freed.");
+
+	log_info("_FreeThread: Done.");
+}
