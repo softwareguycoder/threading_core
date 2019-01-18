fc0938fa5d30d8f2a3a6425a6b4b21e6af3138d1
softwareguycoder <softwareguycoder@gmail.com>
2019-01-18 08:19:54 -0500

    18 Jan 2019 Updated docs for CreateThread

--- threading_core/src/threading_core.c
+++ threading_core/src/threading_core.c
@@ -54,6 +54,7 @@ void _FreeThread(HTHREAD hThread) {
  * @brief Creates a thread to execute within the virtual address space of the calling process.
  * @param lpfnThreadProc (Required.) A pointer to the application-defined function to be executed by the thread.
  * @return Handle to the created thread, or INVALID_HANDLE_VALUE if an error occurred.
+ * @remarks The thread function specified by lpfnThreadProc will begin execution immediately.
  */
 HTHREAD CreateThread(LPTHREAD_START_ROUTINE lpfnThreadProc) {
 	log_info("In CreateThread");
