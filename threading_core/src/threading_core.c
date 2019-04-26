7794c94061e879a2314e41e5a0d323090292348c
softwareguycoder <softwareguycoder@gmail.com>
2019-04-26 07:31:56 -0400

    Changed tabs to spaces, created CancelThread function

--- threading_core/src/threading_core.c
+++ threading_core/src/threading_core.c
@@ -8,21 +8,32 @@
 // meant for internal use only.
 
 void _FreeThread(HTHREAD hThread) {
-	if (INVALID_HANDLE_VALUE == hThread) {
-		// If we have an invalid handle (i.e., NULL pointer),
-		// then there is nothing to do.
-		return;
-	}
+    if (INVALID_HANDLE_VALUE == hThread) {
+        // If we have an invalid handle (i.e., NULL pointer),
+        // then there is nothing to do.
+        return;
+    }
 
-	// The HMUTEX handle type is just a typedef of pthread_mutex_t*
-	// However, to work with the pthread functions, we need to view it
-	// as such.
-	pthread_t* pThread = (pthread_t*) hThread;
+    // The HMUTEX handle type is just a typedef of pthread_mutex_t*
+    // However, to work with the pthread functions, we need to view it
+    // as such.
+    pthread_t* pThread = (pthread_t*) hThread;
 
-	free(pThread);
-	pThread = NULL;
+    free(pThread);
+    pThread = NULL;
 
-	hThread = INVALID_HANDLE_VALUE;
+    hThread = INVALID_HANDLE_VALUE;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// CancelThread function
+
+void CancelThread(HTHREAD hThread) {
+    if (INVALID_HANDLE_VALUE == hThread) {
+        return;
+    }
+
+    pthread_cancel(*((pthread_t*) hThread));
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -30,7 +41,7 @@ void _FreeThread(HTHREAD hThread) {
 // in the current process.  If successful, returns a handle to the new thread.
 
 HTHREAD CreateThread(LPTHREAD_START_ROUTINE lpfnThreadProc) {
-	return CreateThreadEx(lpfnThreadProc, NULL /* pUserState */);
+    return CreateThreadEx(lpfnThreadProc, NULL /* pUserState */);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -38,30 +49,30 @@ HTHREAD CreateThread(LPTHREAD_START_ROUTINE lpfnThreadProc) {
 // current process.  If successful, returns a handle to the new thread.
 
 HTHREAD CreateThreadEx(LPTHREAD_START_ROUTINE lpfnThreadProc,
-		void* __restrict pUserState) {
+        void* __restrict pUserState) {
 
-	/* NOTE: We can't have a thread without a thread procedure function!
-	 * If nothing has been passed for the lpfnThreadProc parameter then that
-	 * is a fatal error. */
+    /* NOTE: We can't have a thread without a thread procedure function!
+     * If nothing has been passed for the lpfnThreadProc parameter then that
+     * is a fatal error. */
 
-	if (NULL == lpfnThreadProc) {
-		// Nothing to do if no thread proc passed
-		return INVALID_HANDLE_VALUE;
-	}
+    if (NULL == lpfnThreadProc) {
+        // Nothing to do if no thread proc passed
+        return INVALID_HANDLE_VALUE;
+    }
 
-	pthread_t* pNewThread = (pthread_t*) malloc(sizeof(pthread_t));
-	if (NULL == pNewThread) {
-		// Failed to allocate memory
-		return INVALID_HANDLE_VALUE;
-	}
+    pthread_t* pNewThread = (pthread_t*) malloc(sizeof(pthread_t));
+    if (NULL == pNewThread) {
+        // Failed to allocate memory
+        return INVALID_HANDLE_VALUE;
+    }
 
-	/* NOTE: A pthread_t* and HTHREAD type are interchangeable */
-	int nResult = pthread_create(pNewThread, NULL, lpfnThreadProc, pUserState);
-	if (OK != nResult) {
-		return INVALID_HANDLE_VALUE;
-	}
+    /* NOTE: A pthread_t* and HTHREAD type are interchangeable */
+    int nResult = pthread_create(pNewThread, NULL, lpfnThreadProc, pUserState);
+    if (OK != nResult) {
+        return INVALID_HANDLE_VALUE;
+    }
 
-	return (HTHREAD) pNewThread;
+    return (HTHREAD) pNewThread;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -69,22 +80,22 @@ HTHREAD CreateThreadEx(LPTHREAD_START_ROUTINE lpfnThreadProc,
 // back to the operating system.
 
 int DestroyThread(HTHREAD hThread) {
-	int nResult = OK;
+    int nResult = OK;
 
-	if (INVALID_HANDLE_VALUE == hThread) {
-		return nResult; /* Nothing to do if thread handle is already
-		 	 	 	 	 	 an invalid value */
-	}
+    if (INVALID_HANDLE_VALUE == hThread) {
+        return nResult; /* Nothing to do if thread handle is already
+         an invalid value */
+    }
 
-	/* Release the memory consumed by the thread handle */
-	_FreeThread(hThread);
+    /* Release the memory consumed by the thread handle */
+    _FreeThread(hThread);
 
-	/* Even though we explicitly called _FreeThread above, let's explicitly
-	 * set the thread handle to INVALID_HANDLE_VALUE just to be on the safe
-	 * side. */
-	hThread = INVALID_HANDLE_VALUE;
+    /* Even though we explicitly called _FreeThread above, let's explicitly
+     * set the thread handle to INVALID_HANDLE_VALUE just to be on the safe
+     * side. */
+    hThread = INVALID_HANDLE_VALUE;
 
-	return nResult;
+    return nResult;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -95,7 +106,7 @@ int DestroyThread(HTHREAD hThread) {
 // be cleaned up, depending on the implementation.
 
 void KillThread(HTHREAD hThread) {
-	KillThreadEx(hThread, SIGSEGV);
+    KillThreadEx(hThread, SIGSEGV);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -104,25 +115,25 @@ void KillThread(HTHREAD hThread) {
 // thread that its lifetime is at an end
 
 void KillThreadEx(HTHREAD hThread, int signum) {
-	if (INVALID_HANDLE_VALUE == hThread) {
-		return;	// Invalid thread handle specified; nothing to do.
-	}
+    if (INVALID_HANDLE_VALUE == hThread) {
+        return;	// Invalid thread handle specified; nothing to do.
+    }
 
-	if (signum <= 0) {
-		return;	// Invalid value for signum; nothing to do.
-	}
+    if (signum <= 0) {
+        return;	// Invalid value for signum; nothing to do.
+    }
 
-	int retval = pthread_kill((pthread_t) (*hThread), signum);
+    int retval = pthread_kill((pthread_t) (*hThread), signum);
 
-	sleep(1); 	// force a context switch to let the thread do its thing
+    sleep(1); 	// force a context switch to let the thread do its thing
 
-	if (OK != retval) {
-		// Failed to kill and/or signal the thread
+    if (OK != retval) {
+        // Failed to kill and/or signal the thread
 
-		perror("KillThreadEx");
+        perror("KillThreadEx");
 
-		exit(ERROR);
-	}
+        exit(ERROR);
+    }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -130,9 +141,9 @@ void KillThreadEx(HTHREAD hThread, int signum) {
 // called when the SIGSEGV signal is raised on a thread.
 
 void RegisterEvent(LPSIGNALHANDLER lpfnEventHandler) {
-	/* This function is just an alias for the RegisterEventEx function
-	 * with a default signum value of SIGSEGV passed. */
-	RegisterEventEx(SIGSEGV, lpfnEventHandler);
+    /* This function is just an alias for the RegisterEventEx function
+     * with a default signum value of SIGSEGV passed. */
+    RegisterEventEx(SIGSEGV, lpfnEventHandler);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -140,26 +151,60 @@ void RegisterEvent(LPSIGNALHANDLER lpfnEventHandler) {
 // when the signal indicated by signum is signalled.
 
 void RegisterEventEx(int signum, LPSIGNALHANDLER lpfnEventHandler) {
-	if (NULL == lpfnEventHandler) {
-		return;	// No event handler function handler specified; nothing to do.
-	}
+    if (NULL == lpfnEventHandler) {
+        return;	// No event handler function handler specified; nothing to do.
+    }
 
-	if (SIG_ERR == signal(signum, lpfnEventHandler)) {
-		// Failed to register event handler
-		perror("RegisterEventEx");
+    if (SIG_ERR == signal(signum, lpfnEventHandler)) {
+        // Failed to register event handler
+        perror("RegisterEventEx");
 
-		exit(ERROR);
-	}
+        exit(ERROR);
+    }
 }
 
+int SetThreadCancelState(int nState) {
+    errno = OK;
+
+    if (nState != PTHREAD_CANCEL_ENABLE && nState != PTHREAD_CANCEL_DISABLE) {
+        return ERROR;
+    }
+
+    int nOldState = OK;
+    if (OK != pthread_setcancelstate(nState, &nOldState)) {
+        fprintf(stderr, "Failed to set thread cancellation state.\n");
+        perror("SetThreadCancelState");
+        exit(ERROR);
+    }
+
+    return nOldState;
+}
+
+int SetThreadCancelType(int nType) {
+    errno = OK;
+
+    if (nType != PTHREAD_CANCEL_DEFERRED && nType != PTHREAD_CANCEL_ASYNCHRONOUS) {
+        errno = EINVAL;
+        return ERROR;
+    }
+
+    int nOldType = OK;
+    if (OK != pthread_setcanceltype(nType, &nOldType)) {
+        fprintf(stderr, "Failed to set thread cancellation type.\n");
+        perror("SetThreadCancelState");
+        exit(ERROR);
+    }
+
+    return nOldType;
+}
 ///////////////////////////////////////////////////////////////////////////////
 // WaitThread: Blocks the calling thread until the specified thread terminates.
 // Does not recover any user state returned by the thread waited upon.
 
 int WaitThread(HTHREAD hThread) {
-	// delegate the implementation to the WaitThreadEx function by passing NULL
-	// for the user state variable.
-	return WaitThreadEx(hThread, NULL);
+    // delegate the implementation to the WaitThreadEx function by passing NULL
+    // for the user state variable.
+    return WaitThreadEx(hThread, NULL);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -167,37 +212,37 @@ int WaitThread(HTHREAD hThread) {
 // thread to have access to any user state returned from the waited-on thread.
 
 int WaitThreadEx(HTHREAD hThread, void **ppvRetVal) {
-	int nResult = ERROR;
+    int nResult = ERROR;
 
-	if (INVALID_HANDLE_VALUE == hThread) {
-		return nResult;	// Invalid thread handle passed; nothing to do.
-	}
+    if (INVALID_HANDLE_VALUE == hThread) {
+        return nResult;	// Invalid thread handle passed; nothing to do.
+    }
 
-	pthread_t *pThread = (pthread_t*) hThread;
+    pthread_t *pThread = (pthread_t*) hThread;
 
-	// pthread_join wants us to dereference the HTHREAD; we can't do this
-	// if pThread is NULL to begin with
-	if (pThread == NULL) {
-		return nResult;
-	}
+    // pthread_join wants us to dereference the HTHREAD; we can't do this
+    // if pThread is NULL to begin with
+    if (pThread == NULL) {
+        return nResult;
+    }
 
-	// get the pthread_t referenced by the handle
-	pthread_t nThreadID = *pThread;
+    // get the pthread_t referenced by the handle
+    pthread_t nThreadID = *pThread;
 
-	nResult = pthread_join(nThreadID, ppvRetVal);
-	if (OK != nResult) {
-		// Failed to join the specified thread.
-		return nResult;
-	}
+    nResult = pthread_join(nThreadID, ppvRetVal);
+    if (OK != nResult) {
+        // Failed to join the specified thread.
+        return nResult;
+    }
 
-	// Once we get here, the thread handle is completely useless, so
-	// free the memory assocaited with it and invalidate the thread
-	// handle.  This is necessary because thread handles are allocated
-	// on the heap.
-	_FreeThread(hThread);
+    // Once we get here, the thread handle is completely useless, so
+    // free the memory assocaited with it and invalidate the thread
+    // handle.  This is necessary because thread handles are allocated
+    // on the heap.
+    _FreeThread(hThread);
 
-	// Pass the result of pthread_join to the caller
-	return nResult;
+    // Pass the result of pthread_join to the caller
+    return nResult;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
